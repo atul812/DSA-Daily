0e9a85b5c5f49c9d866fd591731a00e926ace09a
Atul Kumar <111857956+atul812@users.noreply.github.com>
2026-02-26 07:53:14 +0530

    Implement moveZeroes function to rearrange array

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                temp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = temp
+                i += 1
+
+        return nums
