fbf4b9be522b325869b329fadb4355ac40b5302f
Atul Kumar <111857956+atul812@users.noreply.github.com>
2026-08-11 11:44:29 +0530

    Improve shuffle function implementation
    Refactor shuffle function for better clarity and efficiency.

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -11,3 +11,14 @@ class Solution:
             j += 1
 
         return ans
+
+
+# better approach
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans
